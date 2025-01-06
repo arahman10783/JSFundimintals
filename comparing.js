c21301d37bd1d4353eae645edb3de44743111795
AbdelrahmanElhussiny <arahman1783@yahoo.com>
2025-01-06 20:26:25 +0200

    comparing

---
+++ comparing.js
@@ -0,0 +1,22 @@
+let x = 5
+let y = "5" //typed in input
+
+// Equality
+x == y //true => compare value ignore Type (convert 2 types to string)
+x === y //false => compare value and type
+
+//Not Equality !
+x != y //false => compare value ignore Type (convert 2 types to string)
+x !== y //true => compare value and type
+
+let age = 10
+
+age > 10 // false
+age < 10 // false
+age >= 10 // true
+age <= 10 // true
+
+
+//And &&
+//  "HTML" && "CSS" && "JS" // true => skills must have all skills
+//  "FrontEnd" || "BackEnd" // true => dev should have at least 1 skill
